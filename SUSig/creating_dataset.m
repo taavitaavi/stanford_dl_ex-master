444dfe1f77434a94afd016e9b10273eea6a4374d
taavitaavi <taavitaavi@gmail.com>
2016-01-13 20:18:51 +0200

    converted .sig to suitable format for CNN

--- SUSig/creating_dataset.m
+++ SUSig/creating_dataset.m
@@ -9,28 +9,24 @@ for i=1:length(fullFileNames)
     FileName=fullFileNames{i};
 
     [X Y TStamp Pressure EndPts]=ReadSignature(FileName,ShowSig);
-
-    h=dialog ( 'visible', 'off', 'windowstyle', 'normal' );
-    ax=axes('parent', h);
-    plot (ax,X,-Y,'color','k');
-    set(ax, 'box','off','XTickLabel',[],'XTick',[],'YTickLabel',[],'YTick',[])
-    set(gca,'xcolor','w','ycolor','w','xtick',[],'ytick',[])
-    set(gcf,'color','w');
-    %get filename
-    f=strsplit(FileName,'\');
-    f=f(end);
-    f=strsplit(f{1},'.');
-    f=f{1};
-    saveas (ax,[folder f], 'png' )
-    close(h)
+    n=200;
+    Pressure_res=resample(Pressure,n,length(Pressure));
+    x_res=resample(X,n,length(X));
+    norm_X = round((x_res - min(x_res)) / ( max(x_res) - min(x_res) )*n);
+    y_res=resample(Y,n,length(Y));
+    norm_Y = round((y_res - min(y_res)) / ( max(y_res) - min(y_res) )*n);
+    norm_X(norm_X==0)=1;
+    norm_Y(norm_Y==0)=1;
+    m=zeros(n);
+    norm_Pressure=round((Pressure_res - min(Pressure_res)) / ( max(Pressure_res) - min(Pressure_res) )*255);
+    for j=2:length(norm_X)
+        norm_Pressure(j)
+        m(norm_Y(j),norm_X(j))=norm_Pressure(j);
+        m=func_Drawline(m, norm_Y(j-1), norm_X(j-1), norm_Y(j), norm_X(j), norm_Pressure(j));
+    end
     
-    im=imread([folder f '.png']);
-    BW = im2bw(im, 0.5);
-%     stats = [regionprops(BW); regionprops(not(BW))];
-%     rect=stats(2).BoundingBox;
-%     cropped=imcrop(BW,rect);
-%     imshow(cropped);
-    images(:,:,i)=BW;
+    
+
     
 end
 
